8e62ed98203a79602836cccb8a703436a3752009
Nicolas A <nicolasalexandre0001@gmail.com>
2021-07-07 17:59:54 -0300

    Make logout button

--- src/pages/picturesPage/PicturesPage.js
+++ src/pages/picturesPage/PicturesPage.js
@@ -1,21 +1,41 @@
 import { Button, IconButton } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import PictureCard from "../../components/PictureCard";
 import { useProtectedPage } from "../../hooks/useProtectedPage";
-import { MainContainer, Header, Title, SearchContainer, StyledTextField, PicturesContainer } from "./styled";
+import { goToLogin } from "../../routes/coordinator";
+import {
+  MainContainer,
+  Header,
+  Title,
+  SearchContainer,
+  StyledTextField,
+  PicturesContainer,
+} from "./styled";
 
 const PicturesPage = () => {
   useProtectedPage();
+  const history = useHistory();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    goToLogin(history)
+  };
+
   return (
     <MainContainer>
       <Header>
         <Title>Pic4Share</Title>
         <SearchContainer>
-          <StyledTextField variant='outlined'/>
-          <IconButton><SearchIcon /></IconButton>
+          <StyledTextField variant="outlined" />
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
         </SearchContainer>
-        <Button variant='outlined' color='primary'>Logout</Button>
+        <Button onClick={logout} variant="outlined" color="primary">
+          Logout
+        </Button>
       </Header>
       <PicturesContainer>
         <PictureCard />
