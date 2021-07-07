a5cd0178e1c33e1db49d6c0fdeb700d129b2ae9b
Nicolas A <nicolasalexandre0001@gmail.com>
2021-07-07 16:17:43 -0300

    Make useProtectedPage hook

--- src/pages/createPicturePage/CreatePicturePage.js
+++ src/pages/createPicturePage/CreatePicturePage.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useProtectedPage } from '../../hooks/useProtectedPage';
 import { MainContainer } from './styled';
 
 const CreatePicturePage = () => {
+  useProtectedPage();
   return (
     <MainContainer>teste</MainContainer>
   );
--- src/pages/picturesPage/PicturesPage.js
+++ src/pages/picturesPage/PicturesPage.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useProtectedPage } from '../../hooks/useProtectedPage';
 import { MainContainer } from './styled';
 
 const PicturesPage = () => {
+  useProtectedPage();
   return (
     <MainContainer>teste</MainContainer>
   );
